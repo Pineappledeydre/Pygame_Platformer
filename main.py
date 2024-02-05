0d8a4de53da908cd1fdce93edcf832db61ea2512
Pineappledeydre <89531828+Pineappledeydre@users.noreply.github.com>
2024-02-05 12:35:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+# Import necessary libraries
+import pygame
+# Import the start_level1 function from level_1.py
+from level_1 import start_level1
+
+
+# Window dimensions - playground
+WIDTH = 420
+HEIGHT = 250
+
+# Button dimensions
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 50
+
+# Color
+BLACK = (0, 0, 0)
+
+# Initialize Pygame
+pygame.init()
+
+# Create the main window (where we'll play)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Menu with Buttons")
+
+# Load background image
+background_image = pygame.image.load('background.jpg')
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+
+# Load button images
+button_image = pygame.image.load('button_image.png')
+button_image = pygame.transform.scale(button_image, (BUTTON_WIDTH, BUTTON_HEIGHT))
+
+dark_button_image = pygame.image.load('dark_button_image.png')
+dark_button_image = pygame.transform.scale(dark_button_image, (BUTTON_WIDTH, BUTTON_HEIGHT))
+
+# Create buttons - 6 in total (to choose a level)
+buttons = []
+for i in range(1, 7):
+  x = (i - 1) % 3 * (BUTTON_WIDTH + 10) + 50
+  y = (i - 1) // 3 * (BUTTON_HEIGHT + 10) + 50
+  button_rect = pygame.Rect(x, y, BUTTON_WIDTH, BUTTON_HEIGHT)
+  buttons.append(button_rect)
+print(buttons)
+
+# Main program loop
+running = True
+while running:
+  # Handle events
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+      
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      # Left mouse button clicked
+      if event.button == 1:
+        # Check if the current mouse position (event.pos) is within the area of the current button
+        for i, button in enumerate(buttons):
+          if button.collidepoint(event.pos):
+            # Since Python indexes start from 0 (level 1 will be 0 for python ,etc.)
+            level = i + 1
+            # Execute the corresponding level function
+            exec("start_level" + str(level) + "()")
+
+  # Draw the screen
+  window.blit(background_image, (0, 0))
+
+  # Draw buttons
+  for i, button in enumerate(buttons):
+    if button.collidepoint(pygame.mouse.get_pos()):
+      window.blit(dark_button_image, button)
+    else:
+      window.blit(button_image, button)
+
+    font = pygame.font.SysFont(None, 30)
+    # Add text on the buttons
+    text = font.render(str(i + 1), True, BLACK)
+    text_rect = text.get_rect(center=button.center)
+    window.blit(text, text_rect)
+    
+  # update the content on the window 
+  pygame.display.flip()
