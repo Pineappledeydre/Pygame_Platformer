d9fae1f41dd85675ba7f0185430790812dbb742f
Pineappledeydre <89531828+Pineappledeydre@users.noreply.github.com>
2024-02-18 17:11:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Import necessary libraries
 import pygame
-# Import the start_level1 function from level_1.py
+# knowing that level_1.py contains the start_level1 function
 from level_1 import start_level1
+from level_2 import start_level2
+from replit import audio
 
+# Sound file for button click
+sound = "click.wav"
 
-# Window dimensions - playground
+# Window dimensions
 WIDTH = 420
 HEIGHT = 250
 
@@ -18,7 +22,7 @@ BLACK = (0, 0, 0)
 # Initialize Pygame
 pygame.init()
 
-# Create the main window (where we'll play)
+# Create the main window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu with Buttons")
 
@@ -28,12 +32,14 @@ background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
 # Load button images
 button_image = pygame.image.load('button_image.png')
-button_image = pygame.transform.scale(button_image, (BUTTON_WIDTH, BUTTON_HEIGHT))
+button_image = pygame.transform.scale(button_image,
+                                      (BUTTON_WIDTH, BUTTON_HEIGHT))
 
 dark_button_image = pygame.image.load('dark_button_image.png')
-dark_button_image = pygame.transform.scale(dark_button_image, (BUTTON_WIDTH, BUTTON_HEIGHT))
+dark_button_image = pygame.transform.scale(dark_button_image,
+                                           (BUTTON_WIDTH, BUTTON_HEIGHT))
 
-# Create buttons - 6 in total (to choose a level)
+# Create buttons - 6 in total
 buttons = []
 for i in range(1, 7):
   x = (i - 1) % 3 * (BUTTON_WIDTH + 10) + 50
@@ -49,14 +55,15 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-      
     elif event.type == pygame.MOUSEBUTTONDOWN:
       # Left mouse button clicked
       if event.button == 1:
         # Check if the current mouse position (event.pos) is within the area of the current button
         for i, button in enumerate(buttons):
           if button.collidepoint(event.pos):
-            # Since Python indexes start from 0 (level 1 will be 0 for python ,etc.)
+            print("Button Clicked!")
+            audio.play_file(sound)
+            # Since Python indexes start from 0
             level = i + 1
             # Execute the corresponding level function
             exec("start_level" + str(level) + "()")
@@ -72,10 +79,9 @@ while running:
       window.blit(button_image, button)
 
     font = pygame.font.SysFont(None, 30)
-    # Add text on the buttons
+    # Render text on the button
     text = font.render(str(i + 1), True, BLACK)
     text_rect = text.get_rect(center=button.center)
     window.blit(text, text_rect)
-    
-  # update the content on the window 
+
   pygame.display.flip()
